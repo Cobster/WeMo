95860596b8e72c2e8524ac21c5c578c1ada514d2
Jake Bruun <jake.bruun@gmail.com>
2015-09-25 08:02:33 -0700

    Adds project files

---
+++ src/WeMoLib/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using UPNPLib;
+using WeMoLib.ServiceReference1;
+
+namespace WeMoLib
+{
+    public class WeMoDevice
+    {
+        public string Name { get; set; }
+        public string IpAddress { get; set; }
+        public int Port { get; set; }
+    }
+
+    class WeMoFinder
+    {
+        public WeMoDevice Find(string name)
+        {
+            return Find().FirstOrDefault(x => x.Name == name);
+        }
+
+        private IEnumerable<WeMoDevice> Find()
+        {
+            UPnPDeviceFinder finder = new UPnPDeviceFinder();
+            UPnPDevices found = finder.FindByType("urn:Belkin:device:controllee:1", 0);
+            
+            var devices = found.Cast<UPnPDevice>().Select(device =>
+            {
+                var uri = new Uri(device.PresentationURL);
+
+                return new WeMoDevice
+                {
+                    Name = device.FriendlyName,
+                    IpAddress = uri.Host,
+                    Port = uri.Port
+                };
+            });
+
+            return devices.ToList();
+        }
+    }
+
+    public class WeMoSwitch
+    {
+        public static WeMoSwitch ConnectTo(string deviceName)
+        {
+            var device = new WeMoFinder().Find(deviceName);
+            return device != null ? new WeMoSwitch(device.IpAddress, device.Port) : null;
+        }
+
+        private readonly BasicServicePortTypeClient _client;
+
+        public WeMoSwitch(string ip, int port)
+        {
+            _client = new BasicServicePortTypeClient();
+            _client.Endpoint.Address = new EndpointAddress($"http://{ip}:{port}/upnp/control/basicevent1");
+        }
+
+        public bool IsOff()
+        {
+            var state = _client.GetBinaryState(new GetBinaryState());
+            return state.BinaryState != "1";
+        }
+
+        public void TurnOn()
+        {
+            if (IsOff())
+            {
+                SetState("1");
+            }
+        }
+
+        public void TurnOff()
+        {
+            if (!IsOff())
+            {
+                SetState("0");
+            }
+        }
+
+        private void SetState(string binaryState)
+        {
+            _client.SetBinaryState(new SetBinaryState { BinaryState = binaryState });
+        }
+    }
+}
---
+++ src/WeMoWeb/WeMoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using WeMoLib;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WeMoWeb
+{
+    [Route("/wemo/lamp")]
+    public class WeMoController : Controller
+    {
+        [HttpPost("on")]
+        public void TurnOn()
+        {
+            WeMoSwitch.ConnectTo("Lamp")?.TurnOn();
+        }
+
+        [HttpPost("off")]
+        public void TurnOff()
+        {
+            WeMoSwitch.ConnectTo("Lamp")?.TurnOff();
+        }
+
+        [HttpPost("toggle")]
+        public void Toggle()
+        {
+            var lamp = WeMoSwitch.ConnectTo("Lamp");
+            if (lamp.IsOff())
+            {
+                lamp.TurnOn();
+            }
+            else
+            {
+                lamp.TurnOff();
+            }
+        }
+    }
+}
